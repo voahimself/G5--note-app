1cf9cd2458642b6c4db065c02f86e4158fefd0b4
elvincedik <elvinse3@gmail.com>
2025-01-29 15:38:12 +0100

    #

--- resources/views/note/show.blade.php
+++ resources/views/note/show.blade.php
@@ -2,7 +2,7 @@
     <div>
         <div class="note-container single-note">
             <div class="note-header">
-                <h1 class="text-3xl py-4">{{ $note->created_at }}</h1>
+                <h1 class="py-4 text-3xl">{{ $note->created_at }}</h1>
                 <div class="note-buttons">
                     <a href="#" class="note-edit-button">Edit</a>
                     <button class="note-delete-button">Delete</button>
